251a911e3e8dc0c7622ba99b19f256c01ca30ef1
Théo Lefebvre <theo.lefebvre9@orange.fr>
2022-12-01 02:20:32 +0100

    API REST

--- app/src/main/java/com/example/streamcine/ui/viewmodels/MovieViewModel.kt
+++ app/src/main/java/com/example/streamcine/ui/viewmodels/MovieViewModel.kt
@@ -1,22 +1,27 @@
 package com.example.streamcine.ui.viewmodels
 
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.setValue
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
-import com.example.streamcine.network.MovieDetail
+import com.example.streamcine.data.models.MovieModel
 import com.example.streamcine.network.MoviesApi
 import kotlinx.coroutines.launch
 
 class MovieViewModel : ViewModel() {
 
-    var movies : MutableList<MovieDetail> = mutableListOf()
+    var movies : MutableList<MovieModel> = mutableListOf()
+    var errorMessage : String by mutableStateOf("")
 
     public fun getMovies() {
         viewModelScope.launch {
             try {
-                movies = MoviesApi.retrofitService.getMovies() as MutableList<MovieDetail>
+                val movieList = MoviesApi.retrofitService.getMovies() as MutableList<MovieModel>
+                movies = movieList
             }
             catch(e : Exception) {
-
+                errorMessage = e.message.toString()
             }
         }
     }
