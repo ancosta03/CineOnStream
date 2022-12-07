86bfafe2da4448be0ac9077e5df8ff2288e38354
Théo Lefebvre <theo.lefebvre9@orange.fr>
2022-12-07 20:46:26 +0100

    Revert "Adding of rest preview"
    This reverts commit 56852898e560c6e3172597d754e62f8f89a01a99.

--- app/src/main/java/com/example/streamcine/data/models/MovieModel.kt
+++ app/src/main/java/com/example/streamcine/data/models/MovieModel.kt
@@ -1,4 +1,11 @@
 package com.example.streamcine.data.models
 
-class MovieModel {
-}
+import com.squareup.moshi.Json
+
+data class MovieModel (
+    @Json(name = "name") val name : String,
+    @Json(name = "film_director") val film_director : String,
+    @Json(name = "imageUrl") val imageUrl : String,
+    @Json(name = "desc") val desc : String,
+    @Json(name = "category") val category : String
+)
